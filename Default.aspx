07a4a6d312c5399f84d060a02d22e5d7394430f5
QZenn <evgeny.pogodin@gmail.com>
2012-06-28 12:22:41 +0400

    Initial commit - working ASP.NET Gallery

---
+++ Default.aspx
@@ -0,0 +1,130 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+    <script type="text/javascript">
+        	$(document).ready(function(){
+        		$(".mainblock a").lightBox();
+                var elementheight;
+                var elementwidth;
+                var elementmarginheight;
+                var elementmarginwidth;
+
+                var elementimageheight;
+                var elementimagewidth;
+
+                var elementimagetdheight;
+                var elementimagetdwidth;
+
+	        	$(".element").hover (
+	        		function() {
+
+                        elementheight = $(this).height();
+                        elementwidth = $(this).width();
+                        //console.log("elementheight - " + elementheight);
+                        elementmarginheight = ($(this).outerHeight(true) - $(this).outerHeight())/2;
+                        elementmarginwidth = ($(this).outerWidth(true) - $(this).outerWidth())/2;
+                        
+                        elementimageheight = $(this).find('.elementimage').height();
+                        elementimagewidth = $(this).find('.elementimage').width();
+
+                        elementimagetdheight = $(this).find('.image').height();
+                        elementimagetdwidth = $(this).find('.image').width();
+
+                        var zoompx = 20;
+
+                        var newelementheight = elementheight + zoompx;
+                        var newelementwidth = elementwidth + zoompx;
+                        var newelementmarginheight = elementmarginheight - (zoompx/2);
+                        var newelementmarginwidth = elementmarginwidth - (zoompx/2);
+
+                        var newelementimageheight = elementimageheight + zoompx;
+                        var newelementimagewidth = elementimagewidth + zoompx;
+                        
+                        var newelementimagetdheight;
+                        var newelementimagetdwidth;
+                        if (elementimagetdheight > elementimagetdwidth){
+                            newelementimagetdheight = elementimagetdheight + zoompx;
+                            newelementimagetdwidth = newelementimagetdheight * (elementimagetdwidth/elementimagetdheight);
+                        }else
+                        {
+                            newelementimagetdwidth = elementimagetdwidth + zoompx;
+                            newelementimagetdheight = newelementimagetdwidth * (elementimagetdheight/elementimagetdwidth);
+                        };
+                        
+
+	        			$(this).animate({
+					  		marginTop: newelementmarginheight + "px",
+                            marginBottom: newelementmarginheight + "px",
+                            marginLeft: newelementmarginwidth + "px",
+                            marginRight: newelementmarginwidth + "px",
+					  		height: newelementheight + "px",
+					  		width: newelementwidth + "px",
+	        			}, 100);
+	        			$(this).find('.elementimage').animate({
+	        				height: newelementimageheight + "px",
+	        				width: newelementimagewidth + "px",
+	        			},100);
+	        			$(this).find('.image').animate({
+	        				height: newelementimagetdheight + "px",
+	        				width: newelementimagetdwidth + "px",
+	        			},100);
+					},
+					function() {
+                    console.log("elementheight - " + elementheight);
+						$(this).stop().animate({
+					  		marginTop: elementmarginheight + "px",
+                            marginBottom: elementmarginheight + "px",
+                            marginLeft: elementmarginwidth + "px",
+                            marginRight: elementmarginwidth + "px",
+					  		height: elementheight + "px",
+					  		width: elementwidth + "px",
+	        			}, 50);
+	        			$(this).find('.elementimage').stop().animate({
+	        				height: elementimageheight + "px",
+	        				width: elementimagewidth + "px",
+	        			},50);
+	        			$(this).find('.image').stop().animate({
+	        				height: elementimagetdheight + "px",
+	        				width: elementimagetdwidth + "px",
+	        			},50);
+					}
+	        	);
+        	});
+        </script>
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    
+
+    <asp:Repeater ID="RepeaterThumbnails" runat="server">
+        <ItemTemplate>
+            <div class="elementsurround">
+	    		<a href="<%# Eval("pathToSourceImage") %>" title="<%# Eval("filename")%>">
+			    	<div class="element">
+			    		<table class="elementimage"><tr><td>
+			    			<img class="image" src="<%# Eval("pathToThumbnailImage") %>" alt="<%# Eval("filename")%>">
+			    		</td></tr></table>
+			    		<div class="elementtext">
+			    			<p class="text"><%# Eval("filename") %></p>
+			    		</div>
+			    	</div>
+			    </a>
+	    	</div>
+        </ItemTemplate>
+    </asp:Repeater>
+
+	    		
+	    		<div class="elementsurround">
+	    			<a href="images_for_gallery/img26485-11783311783.jpg" title="Accuracy">
+			    		<div class="element">
+			    			<table class="elementimage"><tr><td>
+			    				<img class="image" src="images_for_gallery/img26485-11783311783.jpg" width="80%">
+			    			</td></tr></table>
+			    			<div class="elementtext">
+			    				<p class="text">Accuracy</p>
+			    			</div>
+			    		</div>
+			    	</a>
+	    		</div>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string pathToThumbnailsFolder = Server.MapPath(@"generated_thumbs\");
+        string pathToSourceFolder = Server.MapPath("images_for_gallery");
+        string[] files = System.IO.Directory.GetFiles(pathToSourceFolder, "*.jpg", System.IO.SearchOption.TopDirectoryOnly);
+
+        int thumbMaxWidth = 180;
+        int thumbMaxHeight = 180;
+        //GenerateThumbnailsForFolder(pathToSourceFolder, pathToThumbnailsFolder, thumbMaxWidth, thumbMaxHeight);
+
+        System.Collections.ArrayList sourceValues = new System.Collections.ArrayList();
+        foreach (string path in files)
+        {
+            System.Drawing.Image image = System.Drawing.Image.FromFile(path);
+            int[] imageDimensionsXY = new int[2];
+            imageDimensionsXY[0] = image.Width;
+            imageDimensionsXY[1] = image.Height;
+            int[] thumbnailSizeXY = CalculateThumbnailSize(imageDimensionsXY, thumbMaxWidth, thumbMaxHeight);
+            System.Drawing.Bitmap bitmap = ResizeImage(image, thumbnailSizeXY[0], thumbnailSizeXY[1]);
+            string filename = (path.Substring(path.LastIndexOf("\\") + 1));
+            string thumbnailPath = (pathToThumbnailsFolder + filename);
+            SaveImageToJpegFile(bitmap, thumbnailPath);
+            bitmap.Dispose();
+            image.Dispose();
+
+            sourceValues.Add(new ThumbnailData(("images_for_gallery/" + filename), ("generated_thumbs/" + filename), filename, thumbnailSizeXY[0], thumbnailSizeXY[1]));
+        }
+        RepeaterThumbnails.DataSource = sourceValues;
+        RepeaterThumbnails.DataBind();
+
+        
+    }
+    public class ThumbnailData
+    {
+        public string pathToSourceImage {get; set;}
+        public string pathToThumbnailImage { get; set; }
+        public string filename { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+
+        public ThumbnailData(string pathToSourceImage, string pathToThumbnailImage, string filename, int width, int height)
+        {
+            this.pathToSourceImage = pathToSourceImage;
+            this.pathToThumbnailImage = pathToThumbnailImage;
+            this.filename = filename;
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+    private void GenerateThumbnailsForFolder(string pathToFolder, string ThumbnailsFolderPath, int thumbMaxWidth, int thumbMaxHeight)
+    {
+        string[] files = Directory.GetFiles(pathToFolder, "*.jpg", SearchOption.TopDirectoryOnly);
+        foreach (string path in files)
+        {
+            System.Drawing.Image image = System.Drawing.Image.FromFile(path);
+            int[] imageDimensionsXY = new int[2];
+            imageDimensionsXY[0] = image.Width;
+            imageDimensionsXY[1] = image.Height;
+            int[] thumbnailSizeXY = CalculateThumbnailSize(imageDimensionsXY, thumbMaxWidth, thumbMaxHeight);
+            System.Drawing.Bitmap bitmap = ResizeImage(image, thumbnailSizeXY[0], thumbnailSizeXY[1]);
+            string filename = (path.Substring(path.LastIndexOf("\\") + 1));
+            string thumbnailPath = (ThumbnailsFolderPath + filename);
+            SaveImageToJpegFile(bitmap, thumbnailPath);
+            bitmap.Dispose();
+            image.Dispose();
+        }
+    }
+
+    private static int[] CalculateThumbnailSize(int[] imageDimensionsXY, int thumbMaxWidth, int thumbMaxHeight)
+    {
+        int srcWidth = imageDimensionsXY[0];
+        int srcHeight = imageDimensionsXY[1];
+        int thumbHeight;
+        int thumbWidth;
+        if (srcHeight > srcWidth)
+        {
+            thumbHeight = (int)thumbMaxHeight;
+            thumbWidth = (int)((double)thumbMaxHeight * ((double)srcWidth / (double)srcHeight));
+
+        }
+        else
+        {
+            thumbWidth = (int)thumbMaxWidth;
+            thumbHeight = (int)((double)thumbMaxWidth * ((double)srcHeight / (double)srcWidth));
+        }
+        int[] thumbnailSizeXY = new int[2];
+        thumbnailSizeXY[0] = thumbWidth;
+        thumbnailSizeXY[1] = thumbHeight;
+
+        return thumbnailSizeXY;
+    }
+
+    private static void SaveImageToJpegFile(System.Drawing.Bitmap bitmap, string path)
+    {
+        System.Drawing.Imaging.ImageCodecInfo imagecodec;
+        System.Drawing.Imaging.EncoderParameters encoderparams;
+        imagecodec = GetEncoderInfo("image/jpeg");
+        encoderparams = new System.Drawing.Imaging.EncoderParameters(1);
+        encoderparams.Param[0] = new System.Drawing.Imaging.EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 100L);
+        bitmap.Save(path, imagecodec, encoderparams);
+    }
+
+    /// <summary>
+    /// Returns Bitmap with high quality image resize.
+    /// </summary>
+    /// <param name="image">Image object</param>
+    /// <param name="requiredWidth">Horizontal size</param>
+    /// <param name="requiredHeight">Vertical size</param>
+    /// <returns></returns>
+    private static System.Drawing.Bitmap ResizeImage(System.Drawing.Image image, int requiredWidth, int requiredHeight)
+    {
+        int srcWidth = image.Width;
+        int srcHeight = image.Height;
+        System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(requiredWidth, requiredHeight);
+        System.Drawing.Graphics graphics = System.Drawing.Graphics.FromImage(bitmap);
+        graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+        graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+        graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+        System.Drawing.Rectangle rectangle = new System.Drawing.Rectangle(0, 0, requiredWidth, requiredHeight);
+        graphics.DrawImage(image, rectangle, 0, 0, srcWidth, srcHeight, System.Drawing.GraphicsUnit.Pixel);
+        return bitmap;
+    }
+
+    private static ImageCodecInfo GetEncoderInfo(String mimeType)
+    {
+        int j;
+        ImageCodecInfo[] encoders;
+        encoders = ImageCodecInfo.GetImageEncoders();
+        for (j = 0; j < encoders.Length; ++j)
+        {
+            if (encoders[j].MimeType == mimeType)
+                return encoders[j];
+        }
+        return null;
+    }
+}
